2f2f19a347a098573382de1c0fd0e524bf3830ab
Batu Özçamlık <bozcamlik@gmail.com>
2023-07-08 20:40:28 +0300

    son hali

---
+++ Assets/Script/UI/Parchment/ParchmentMng.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParchmentMng : MonoBehaviour
+{
+    public bool canOpen = false;
+    public SpriteRenderer spriteRender;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canOpen==true)
+        {
+            //spriteRender.size=Mathf.Lerp()
+        }
+    }
+}
