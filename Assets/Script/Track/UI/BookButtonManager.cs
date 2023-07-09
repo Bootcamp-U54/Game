7b8d709613555f3740ac3483bce0073a5a325369
AhmTD <urbanrivas45@gmail.com>
2023-07-09 18:33:57 +0300

    init
    Kitap revize

---
+++ Assets/Script/Track/UI/BookButtonManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BookButtonManager : MonoBehaviour
+{
+    public Image mainImage,storyImage, trackImage, controllerImage;
+    public GameObject storyBook, trackBook, controllerBook;
+
+
+    private void Start()
+    {
+        SetAllImageVisibility(false);
+        SetAllBookActive(false);
+        mainImage.enabled = true;
+    }
+    public void OnClickStoryButton()
+    {
+        SetAllImageVisibility(false);
+        SetAllBookActive(false);
+
+        storyImage.enabled = true;
+        storyBook.SetActive(true);
+    }
+
+    public void OnClickTrackButton()
+    {
+        SetAllImageVisibility(false);
+        SetAllBookActive(false);
+
+        trackImage.enabled = true;
+        trackBook.SetActive(true);
+    }
+
+    public void OnClickControllerButton()
+    {
+        SetAllImageVisibility(false);
+        SetAllBookActive(false);
+
+        controllerImage.enabled = true;
+        controllerBook.SetActive(true);
+    }
+
+    private void SetAllImageVisibility(bool visible)
+    {
+        mainImage.enabled = visible;
+        storyImage.enabled = visible;
+        controllerImage.enabled = visible;
+        trackImage.enabled = visible;
+    }
+
+    private void SetAllBookActive(bool active)
+    {
+        storyBook.SetActive(active);
+        controllerBook.SetActive(active);
+        trackBook.SetActive(active);
+    }
+
+
+
+}
--- Assets/Script/Track/UI/BookManager.cs
+++ Assets/Script/Track/UI/BookManager.cs
@@ -7,59 +7,104 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using static System.Net.Mime.MediaTypeNames;
-
 public class BookManager : MonoBehaviour
 {
-    PauseMenuManager pauseMenuManager;
     public TextMeshProUGUI[] page;
-    public float fadeDuration = 2f;
-    public int currentIndex = 0;
     public GameObject bookCanvas;
+    public float fadeDuration = 2f;
 
+    private int currentIndex = 0;
+    private bool isTyping = false;
+    private float typingSpeed;
+    private bool isPaused = false;
 
     private void Start()
     {
         for (int i = 1; i < page.Length; i++)
         {
-            page[i].DOFade(0f, 0f);
+            page[i].DOFade(0f, 0f).SetUpdate(true);
         }
-
-        
     }
+
+
     private void Update()
     {
-       
-        if (Input.GetKeyDown(KeyCode.B) )
+        if (Input.GetKeyDown(KeyCode.B))
         {
-            Cursor.visible = true; // Fare imleci görünür hale gelsin
-            Cursor.lockState = CursorLockMode.None; // Fare imleci ekranda serbestçe hareket edebilsin
-            bookCanvas.SetActive(true);
-        
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+                bookCanvas.SetActive(true);
+                StartTypewriterEffect(page[currentIndex].text);
+                Time.timeScale = 0f;
+                isPaused = true;
+
+            }
         }
+    }
+
+    public void OnClickStoryText()
+    {
+        StartTypewriterEffect(page[currentIndex].text);
     }
     public void OnClickNextPage()
     {
-        if (currentIndex >= 0 && currentIndex + 1 < page.Length)
+        if (!isTyping && currentIndex >= 0 && currentIndex + 1 < page.Length)
         {
-            page[currentIndex].DOFade(0f, fadeDuration);
-            page[currentIndex + 1].DOFade(1f, fadeDuration);
+            page[currentIndex].DOFade(0f, fadeDuration).SetUpdate(true);
+            page[currentIndex + 1].DOFade(1f, fadeDuration).SetUpdate(true);
             currentIndex++;
+            StartTypewriterEffect(page[currentIndex].text);
         }
     }
+
     public void OnClickBackpage()
     {
-        if (currentIndex > 0)
+        if (!isTyping && currentIndex > 0)
         {
-            page[currentIndex].DOFade(0f, fadeDuration);
-            page[currentIndex - 1].DOFade(1f, fadeDuration);
+            page[currentIndex].DOFade(0f, fadeDuration).SetUpdate(true);
+            page[currentIndex - 1].DOFade(1f, fadeDuration).SetUpdate(true);
             currentIndex--;
+            StartTypewriterEffect(page[currentIndex].text);
         }
     }
+
+    private void StartTypewriterEffect(string text)
+    {
+        if (!isTyping)
+        {
+            isTyping = true;
+            StartCoroutine(TypeText(text));
+        }
+    }
+
+    private IEnumerator TypeText(string text)
+    {
+        TextMeshProUGUI currentPage = page[currentIndex];
+        currentPage.text = "";
+
+        foreach (char character in text)
+        {
+            currentPage.text += character;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+
+        isTyping = false;
+    }
+
     public void ResumeGame()
     {
         bookCanvas.SetActive(false);
-        Time.timeScale = 1f; // Oyun zaman ölçeðini 1 olarak ayarla, oyun devam etsin
-        Cursor.visible = false; // Fare imleci gizlensin
-        Cursor.lockState = CursorLockMode.Locked; // Fare imleci ekranda sabitlensin
+        Time.timeScale = 1f;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        Time.timeScale = 1f;
+        isPaused = false;
     }
 }
