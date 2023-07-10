b2ebb0ffef562e369297305c916479a81e6d6ddb
Batu Özçamlık <bozcamlik@gmail.com>
2023-07-11 00:24:00 +0300

    Hikaye sahnesi bitti

--- Assets/Script/UI/StoryScene/StoryMng.cs
+++ Assets/Script/UI/StoryScene/StoryMng.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using TMPro;
 using DG.Tweening;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 public class StoryMng : MonoBehaviour
 {
     public GameObject[] page;
@@ -15,6 +15,8 @@ public class StoryMng : MonoBehaviour
     public float writeDuration;
     public float fillAmountValue;
     public bool fillImage = false;
+    public Image black;
+    public TextMeshProUGUI canPast;
 
     private void Start()
     {
@@ -33,7 +35,7 @@ public class StoryMng : MonoBehaviour
     {
         for (int i = 0; i < page.Length; i++)
         {
-            
+            canPast.DOFade(0, 1f);
             text = page[i].transform.GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
             draw = page[i].transform.GetChild(1).gameObject.GetComponent<Image>();
 
@@ -48,15 +50,25 @@ public class StoryMng : MonoBehaviour
                 text.text += a.ToString();
                 yield return new WaitForSeconds(writeDuration);
             }
-
+            canPast.DOFade(1, 1f);
             yield return new WaitUntil(() => Input.anyKeyDown);
 
-            text.DOFade(0, 1f);
-            draw.DOFade(0, 1f);
-            yield return new WaitForSeconds(1);
+            if(page.Length-1!=i)
+            {
 
-            resetAll();
+                text.DOFade(0, 1f);
+                draw.DOFade(0, 1f);
+                yield return new WaitForSeconds(1);
+
+                resetAll();
+            }
         }
+        /*
+        this.gameObject.transform.DOScale(new Vector3(10, 10, 10), 2f);
+        text.gameObject.transform.DOScale(new Vector3(10, 10, 10), 2f);
+        draw.gameObject.transform.DOScale(new Vector3(draw.gameObject.transform.localScale.x*10, draw.gameObject.transform.localScale.y * 10, draw.gameObject.transform.localScale.z * 10), 2f);
+        */
+        black.DOFade(1, 1f).OnComplete(()=>SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1));
        
         
     }
