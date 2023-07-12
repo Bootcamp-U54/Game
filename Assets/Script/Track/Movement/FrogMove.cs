259237ec6ba72ab4af7e4fc42dfcb836c7c6daca
Batu Özçamlık <bozcamlik@gmail.com>
2023-07-12 22:22:42 +0300

    Olum ekrani bari ve parkur 1 UI duzenlendi

--- Assets/Script/Track/Movement/FrogMove.cs
+++ Assets/Script/Track/Movement/FrogMove.cs
@@ -16,6 +16,7 @@ public class FrogMove : MonoBehaviour
     public float maxXPos;
     public Animator anim;
 
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -39,6 +40,7 @@ public class FrogMove : MonoBehaviour
         currentJump = Random.Range(minJump, maxJump);
         // Sadece sola doðru zýplamasý için yatay kuvvet uygula
         rb.velocity = new Vector2(-speed, currentJump);
+        GetComponent<AudioSource>().Play();
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
