1685f0c541e84f06599440790e93b7935c29a95e
AhmTD <urbanrivas45@gmail.com>
2023-07-12 08:37:10 +0300

    init

--- Assets/Script/Track/UI/BookManager.cs
+++ Assets/Script/Track/UI/BookManager.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -13,36 +14,77 @@ public class BookManager : MonoBehaviour
 {
     TextMeshProUGUI currentPage;
     public TextMeshProUGUI[] storyPage;
-    public Image[]  trackPage, controlerPage;
+    public Image[] trackPage, controlerPage;
     public GameObject bookCanvas;
     public float fadeDuration = 2f;
-    private int currentIndexStory,currentIndexTrack,currentIndexController = 0;
+    private int currentIndexStory;
+
+    private int catBorder = 1;
+    private int skeletorBorder = 7;
+
+    private int currentIndexTrack, currentIndexController = 0;
     private bool isTyping = false;
-    private float typingSpeed;
+    public float typingSpeed;
     private bool isPaused = false;
 
+    int ScaneIndex;
 
     [Header("BookAnim")]
     public GameObject Book;
     public Transform startTransform;
     public Transform backTransform;
     public float animationDuration = 1f;
+
+
     private void Start()
     {
+        ScaneIndex = UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex;
+
+        if (ScaneIndex == 3 || ScaneIndex == 4)
+        {
+            currentIndexStory = 0;
+
+        }
+
+        else if (ScaneIndex == 5 || ScaneIndex == 6)
+        {
+            currentIndexStory = 2;
+            for (int i = 0; i < currentIndexStory; i++)
+            {
+                storyPage[i].DOFade(0f, 0f).SetUpdate(true);
+            }
+
+
+
+        }
+        else if (ScaneIndex >= 7)
+        {
+            currentIndexStory = 8;
+
+            for (int i = 0; i < currentIndexStory; i++)
+            {
+                storyPage[i].DOFade(0f, 0f).SetUpdate(true);
+            }
+        }
+
+        else if (ScaneIndex >= 7)
+            currentIndexStory = 7;
+
+
         for (int i = 1; i < storyPage.Length; i++)
         {
             storyPage[i].DOFade(0f, 0f).SetUpdate(true);
- 
+
         }
         for (int i = 1; i < trackPage.Length; i++)
         {
             trackPage[i].DOFade(0f, 0f).SetUpdate(true);
- 
+
         }
-         for (int i = 1; i < controlerPage.Length; i++)
+        for (int i = 1; i < controlerPage.Length; i++)
         {
             controlerPage[i].DOFade(0f, 0f).SetUpdate(true);
- 
+
         }
 
     }
@@ -50,8 +92,8 @@ public class BookManager : MonoBehaviour
 
     private void Update()
     {
+        Debug.Log(currentIndexStory);
 
-        
         if (Input.GetKeyDown(KeyCode.B))
         {
             if (isPaused)
@@ -65,6 +107,8 @@ public class BookManager : MonoBehaviour
                 Cursor.visible = true;
                 Cursor.lockState = CursorLockMode.None;
                 ShowPanel();
+                storyPage[currentIndexStory].DOFade(1f, 0f).SetUpdate(true);
+                StartCoroutine(TypeText(storyPage[currentIndexStory].text));
                 Time.timeScale = 0f;
                 isPaused = true;
 
@@ -72,34 +116,60 @@ public class BookManager : MonoBehaviour
         }
     }
 
-    public void OnClickStoryText()
-    {
-        StartTypewriterEffect(storyPage[currentIndexStory].text);
-    }
-
-
-
-
-
     public void OnClickNextPage(int index)
     {
-        if (!isTyping && currentIndexStory >= 0 && currentIndexStory + 1 < storyPage.Length && index == 0)
+
+        #region StoryGenerator 
+        if (ScaneIndex == 3 || ScaneIndex == 4)
         {
-            storyPage[currentIndexStory].DOFade(0f, fadeDuration).SetUpdate(true);
-            storyPage[currentIndexStory + 1].DOFade(1f, fadeDuration).SetUpdate(true);
-            currentIndexStory++;
-            StartTypewriterEffect(storyPage[currentIndexStory].text);
+            if (!isTyping && currentIndexStory >= 0 && currentIndexStory < catBorder && index == 0)
+            {
+
+                storyPage[currentIndexStory].DOFade(0f, fadeDuration).SetUpdate(true);
+                storyPage[currentIndexStory + 1].DOFade(1f, fadeDuration).SetUpdate(true);
+                currentIndexStory++;
+                StartTypewriterEffect(storyPage[currentIndexStory].text);
+
+            }
         }
+
+        else if (ScaneIndex == 5 || ScaneIndex == 6)
+        {
+            if (!isTyping && currentIndexStory >= 0 && currentIndexStory < skeletorBorder && index == 0)
+            {
+
+                storyPage[currentIndexStory].DOFade(0f, fadeDuration).SetUpdate(true);
+                storyPage[currentIndexStory + 1].DOFade(1f, fadeDuration).SetUpdate(true);
+                currentIndexStory++;
+                StartTypewriterEffect(storyPage[currentIndexStory].text);
+
+            }
+        }
+
+        else if (ScaneIndex >= 7)
+        {
+            if (!isTyping && currentIndexStory >= 0 && currentIndexStory + 1 < storyPage.Length && index == 0)
+            {
+
+                storyPage[currentIndexStory].DOFade(0f, fadeDuration).SetUpdate(true);
+                storyPage[currentIndexStory + 1].DOFade(1f, fadeDuration).SetUpdate(true);
+                currentIndexStory++;
+                StartTypewriterEffect(storyPage[currentIndexStory].text);
+
+            }
+        }
+
+        #endregion
 
         if (!isTyping && currentIndexTrack >= 0 && currentIndexTrack + 1 < trackPage.Length && index == 1)
         {
             trackPage[currentIndexTrack].DOFade(0f, fadeDuration).SetUpdate(true);
             trackPage[currentIndexTrack + 1].DOFade(1f, fadeDuration).SetUpdate(true);
             currentIndexTrack++;
-          
+
         }
 
-         if (!isTyping && currentIndexTrack >= 0 && currentIndexTrack + 1 < trackPage.Length && index == 2)
+        if (!isTyping && currentIndexTrack >= 0 && currentIndexTrack + 1 < trackPage.Length && index == 2)
         {
             controlerPage[currentIndexTrack].DOFade(0f, fadeDuration).SetUpdate(true);
             controlerPage[currentIndexTrack + 1].DOFade(1f, fadeDuration).SetUpdate(true);
@@ -125,7 +195,7 @@ public class BookManager : MonoBehaviour
             trackPage[currentIndexTrack - 1].DOFade(1f, fadeDuration).SetUpdate(true);
             currentIndexTrack--;
         }
-         if (!isTyping && currentIndexController > 0 && index == 2)
+        if (!isTyping && currentIndexController > 0 && index == 2)
         {
             controlerPage[currentIndexController].DOFade(0f, fadeDuration).SetUpdate(true);
             controlerPage[currentIndexController - 1].DOFade(1f, fadeDuration).SetUpdate(true);
@@ -145,13 +215,13 @@ public class BookManager : MonoBehaviour
         {
             isTyping = true;
             StartCoroutine(TypeText(text));
-          
+
         }
     }
 
     private IEnumerator TypeText(string text)
     {
-      
+
         currentPage = storyPage[currentIndexStory];
         currentPage.text = "";
 
